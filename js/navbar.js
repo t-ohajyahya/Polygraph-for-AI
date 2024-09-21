2a683bddd2382c13d165fbc1c45f110d8e880536
REDMOND\t-dibtibi <t-dibtibi@microsoft.com>
2024-09-21 17:06:18 +0300

    fixed routing bug

--- js/navbar.js
+++ js/navbar.js
@@ -1,21 +1,7 @@
 document.getElementById('inspectBtn').onclick = function() {
-        // Get the current URL
-        let currentUrl = window.location.href;
-    
-        // Use regex to replace the last segment
-        let newUrl = currentUrl.replace(/\/[^\/]*$/, '/inspect.html');
-    
-        // Change the window location to the new URL
-        window.location.href = newUrl;
+    window.location.href = "https://t-ohajyahya.github.io/Polygraph-for-AI/index.html"
 };
 
 document.getElementById('homeBtn').onclick = function() {
-            // Get the current URL
-            let currentUrl = window.location.href;
-    
-            // Use regex to replace the last segment
-            let newUrl = currentUrl.replace(/\/[^\/]*$/, '/index.html');
-        
-            // Change the window location to the new URL
-            window.location.href = newUrl;
+    window.location.href = "https://t-ohajyahya.github.io/Polygraph-for-AI/inspect.html"
 };
