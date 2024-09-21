70adee79b5251c8f7cb96d2d562db21ad3d681d5
REDMOND\t-dibtibi <t-dibtibi@microsoft.com>
2024-09-21 17:07:49 +0300

    fixed routing bug

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 document.getElementById('inspectBtn').onclick = function() {
-    window.location.href = "https://t-ohajyahya.github.io/Polygraph-for-AI/index.html"
+    window.location.href = "https://t-ohajyahya.github.io/Polygraph-for-AI/inspect.html"
 };
 
 document.getElementById('homeBtn').onclick = function() {
-    window.location.href = "https://t-ohajyahya.github.io/Polygraph-for-AI/inspect.html"
+    window.location.href = "https://t-ohajyahya.github.io/Polygraph-for-AI/index.html"
 };
